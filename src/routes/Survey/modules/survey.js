6637d613af7b4b46f18abf42c9e0bf80d7c30cd4
Ying Cong <yingcong@glints.com>
2017-03-23 18:47:30 +0800

    front-end implemented

--- src/routes/Survey/modules/survey.js
+++ src/routes/Survey/modules/survey.js
@@ -1,39 +1,44 @@
-// ------------------------------------
-// Constants
-// ------------------------------------
 export const REQUEST_QUESTIONS = 'REQUEST_QUESTIONS'
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
-export const SUBMIT_ANSWERS = 'SUBMIT_ANSWERS'
 export const SUBMITTING_ANSWERS = 'SUBMITTING_ANSWERS'
 export const SUBMITTED_ANSWERS = 'SUBMITTED_ANSWERS'
 export const SELECT_ACTIVE_QUESTION = 'SELECT_ACTIVE_QUESTION'
 export const SAVE_ANSWER = 'SAVE_ANSWER'
+export const SAVE_ANSWER_ERROR= 'SAVE_ANSWER_ERROR'
+export const ADD_OPTION = 'ADD_OPTION'
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function requestQuestions () {
+export function requestQuestions() {
   return {
     type: REQUEST_QUESTIONS
   }
 }
 
-export function receiveQuestions (json) {
+export function receiveQuestions(json) {
   return {
     type: RECEIVE_QUESTIONS,
     payload: json
   }
 }
 
-export function submittingAnswers () {
+export function saveAnswerError(error) {
+  return {
+    type: SAVE_ANSWER_ERROR,
+    payload: error
+  }
+}
+
+export function submittingAnswers() {
   return {
     type: SUBMITTING_ANSWERS
   }
 }
 
-export function submittedAnswers () {
+export function submittedAnswers() {
   return {
-    typeof: SUBMITTED_ANSWERS
+    type: SUBMITTED_ANSWERS
   }
 }
 
@@ -41,42 +46,50 @@ export const submitAnswers = (answers) => {
   return dispatch => {
     return new Promise(() => {
       const data = new FormData()
-      data.append("json", JSON.stringify(answers))
+      data.append('json', JSON.stringify(answers))
       dispatch(submittingAnswers())
-      fetch('http://localhost:2001', {
+      fetch('http://localhost:2001/answers', {
         method: 'POST',
         body: data
       })
-      .then(res => {
-        dispatch(submittedAnswers())
-      })
+        .then(() => {
+          dispatch(submittedAnswers())
+        })
     })
   }
 }
 
-export function selectActiveQuestion (questionId) {
+export function selectActiveQuestion(questionId) {
   return {
     type: SELECT_ACTIVE_QUESTION,
     payload: questionId
   }
 }
 
-export function saveAnswer (answer) {
+export function saveAnswer(answer) {
   return {
     type: SAVE_ANSWER,
     payload: answer
   }
 }
 
+export function addOption(option) {
+  return {
+    type: ADD_OPTION,
+    payload: option
+  }
+}
+
 export const fetchQuestions = () => {
   return dispatch => {
+    dispatch(requestQuestions())
     return new Promise(() => {
-      dispatch(requestQuestions())
-      setTimeout(() => {
-        const json = require('../../../../questions.json')
-        dispatch(receiveQuestions(json))
-        return
-      }, 1000)
+      fetch('http://localhost:2001/questions')
+        .then(res => {
+          res.json().then(response => {
+            return dispatch(receiveQuestions(response))
+          })
+        })
     })
   }
 }
@@ -84,28 +97,29 @@ export const fetchQuestions = () => {
 export const actions = {
   requestQuestions,
   receiveQuestions,
+  saveAnswerError,
   submittingAnswers,
   submittedAnswers,
   selectActiveQuestion,
   fetchQuestions,
-  saveAnswer
+  saveAnswer,
+  addOption
 }
-
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [REQUEST_QUESTIONS] : state => ({
+  [REQUEST_QUESTIONS]: state => ({
     ...state,
     fetching: true
   }),
-  [RECEIVE_QUESTIONS] : (state, action) => ({
+  [RECEIVE_QUESTIONS]: (state, action) => ({
     ...state,
     title: action.payload.title,
     questions: action.payload.questions,
     fetching: false
   }),
-  [SELECT_ACTIVE_QUESTION] : (state, action) => {
+  [SELECT_ACTIVE_QUESTION]: (state, action) => {
     let payload = action.payload
     const numberOfQuestions = state.questions.length
     if (action.payload <= 0) {
@@ -118,20 +132,38 @@ const ACTION_HANDLERS = {
       activeQuestionId: payload
     }
   },
-  [SUBMITTING_ANSWERS] : (state, action) => ({
+  [SUBMITTING_ANSWERS]: (state) => ({
     ...state,
     submitting: true
   }),
-  [SUBMITTING_ANSWERS] : (state, action) => ({
+  [SUBMITTING_ANSWERS]: (state) => ({
     ...state,
     submitting: false,
     submitted: true
   }),
-  [SAVE_ANSWER] : (state, action) => ({
+  [SAVE_ANSWER]: (state, action) => ({
     ...state,
     questions: state.questions.map(qn => {
       if (qn.index === action.payload.questionId) {
         qn.answer = action.payload.answer
+      }
+      return qn
+    })
+  }),
+  [SAVE_ANSWER_ERROR]: (state, action) => ({
+    ...state,
+    questions: state.questions.map(qn => {
+      if (qn.index === action.payload.questionId) {
+        qn.error = action.payload.error
+      }
+      return qn;
+    })
+  }),
+  [ADD_OPTION]: (state, action) => ({
+    ...state,
+    questions: state.questions.map(qn => {
+      if (qn.index === action.payload.questionId && Array.isArray(qn.options)) {
+        qn.options.push(action.payload.option)
       }
       return qn;
     })
@@ -147,7 +179,7 @@ const initialState = {
   activeQuestionId: 0
 }
 
-export default function surveyReducer (state = initialState, action) {
+export default function surveyReducer(state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
 
   return handler ? handler(state, action) : state
